77c3f926f98dc50c779299213736315d34182703
shizuku13 <caramqbtw@gmail.com>
2026-01-14 21:15:52 +0200

    logic code

--- main.py
+++ main.py
@@ -1,16 +1,53 @@
-# This is a sample Python script.
+import os
+import requests
+from dotenv import load_dotenv
 
-# Press Ctrl+F5 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+load_dotenv()
+
+#Read API-key
+API_KEY = os.getenv("API_KEY")
+
+#chek API-key
+if not API_KEY:
+    print("API key not set")
+    exit(1)
+else:
+    print("API key set")
+
+#take user input
+base_currency = input("Put base currency: ").upper()
+target_currency = input("Put target currency: ").upper()
+amount = float(input("Put amount: "))
+
+#formating request
+url = f"https://v6.exchangerate-api.com/v6/{API_KEY}/latest/{base_currency}"
+#url = f"https://v6.exchangerate-api.com/v6/{API_KEY}/latest/EUR"
+#url = f"https://v6.exchangerate-api.com/v6/{API_KEY}/latest/pair/USD/UAH"
+response = requests.get(url)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press F9 to toggle the breakpoint.
+#check response API
+if response.status_code == 200:
+    print("API good")
+    data = response.json()
 
+#chack request status
+    if data["result"] == "success":
+        print("API good")
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+        #check currency in response
+        if target_currency in data["conversion_rates"]:
+            rate = data["conversion_rates"][target_currency]
+            converted_amount = amount * rate
+            print(f"{amount} {base_currency} = {converted_amount:.2f} {target_currency}")
+        else:
+            print("Error. Not currency.")
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    else:
+        print("API dosen't work")
+
+else:
+    print("API error")
+    print(f"Status code: {response.status_code}")
+    print(f"Response: {response.text}")
+
